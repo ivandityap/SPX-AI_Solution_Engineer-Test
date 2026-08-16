23ec622f6cc535cd044f392b7ac638b9c48ec1f0
ivan arsyaditya <ivannanda93@gmail.com>
2026-08-16 15:21:07 +0700

    OCR Extractor functionality (Tess+Gemini)

--- Coding05-UI/app.py
+++ Coding05-UI/app.py
@@ -6,6 +6,12 @@ from concurrent.futures import ThreadPoolExecutor
 import datetime
 import textwrap
 import time
+import pytesseract
+from PIL import Image
+from google import genai
+from google.genai import types
+import os
+from dotenv import load_dotenv
 
 st.set_page_config(page_title="Receipt Assistant", page_icon="🧾")
 
@@ -20,8 +26,46 @@ MAX_FILE_SIZE = 10 * 1024 * 1024  # 10MB
 # Max dimensions for processing
 MAX_IMAGE_SIZE = 2000  # pixels
 
-def ocr_image():
+ocr_extract_system_prompt = """
+Your main task is to extract key information from an OCR result. The processed content is mainly a receipt. You must extract these information:
+1. Receipt Date (Use DD-MM-YYYY Format)
+2. Item Name
+3. Price
+
+After you retrieve those information, I want you to create a structured format to inject the information to an SQLlite table with column:
+1. date
+2. item
+3. price
+
+Use this structure as a guide:
+
+
+ONLY ANSWER WITH A STRUCTURE LIKE THIS:
+(Receipt Date, Item Name, Price)
+"""
+model = "gemini-3.5-flash-lite"
+
+load_dotenv()
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=GEMINI_API_KEY)
+
+
+
+
+def ocr_reasoner(extracted_text):
+    response = client.models.generate_content(
+        model=model,
+        contents=extracted_text,
+        config=types.GenerateContentConfig(system_instruction=ocr_extract_system_prompt),
+        )
+    print(response.text)
+    st.success(response.text)
+
+def ocr_image(receipt):
     st.success("Form submitted successfully!")
+    image = Image.open(receipt)
+    extracted_text = pytesseract.image_to_string(image, lang="ind", config='--psm 6')
+    return extracted_text
 
 def configure_sidebar() -> None:
     """
@@ -46,7 +90,8 @@ def configure_sidebar() -> None:
             submitted = st.form_submit_button(
                 "Submit", type="primary", use_container_width=True)
             if submitted and my_upload is not None:
-                ocr_image()
+                extracted_text = ocr_image(my_upload)
+                ocr_reasoner(extracted_text)
             elif submitted:
                 st.warning("Please upload an image before submitting.")
             
